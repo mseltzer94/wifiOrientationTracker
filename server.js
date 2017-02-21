2eeb06f6e9a65e67acb5955a01afadafb7e613ff
Michael Seltzer <michael.m.seltzer@gmail.com>
2017-02-21 16:28:15 +0000

    new library for yaw, pitch, roll

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-var express = require('express')
-var i2c = require('i2c-bus');
-var MPU6050 = require('i2c-mpu6050')
-var app = express()
-
-var address = 0x68;
-var i2c1 = i2c.openSync(1);
-var sensor = new MPU6050(i2c1,address);
-
-var yaw = 0;
+var express = require('express');
+var mpu = require('mpu6050-dmp');
+var app = express();
 
 var data;
-setInterval(function(){
-	data = sensor.readSync();
-	console.log(data)
-}, 5);
+if (mpu.initialize()){
+	setInterval(function(){
+		data = mpu.getRotation();
+		console.log(data)
+	}, 5);
+}
+
 
 
 app.get('/', function (req, res) {
-  res.send(data.rotation.x.toString());
+	res.send(data);
 })
 
 app.listen(3000, function () {
-  	console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!')
 })
 
