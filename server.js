98715736fc0318693e9d3fe12b7a9e2e9278464c
Michael Seltzer <michael.m.seltzer@gmail.com>
2017-03-19 22:45:17 +0000

    emit double value

--- server.js
+++ server.js
@@ -5,30 +5,40 @@ var app = express();
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 io.on('connection', function(client){
-  client.on('event', function(data){});
-  client.on('disconnect', function(){});
-  client.on('connect', function(){
+	client.on('event', function(data){});
+	client.on('disconnect', function(){});
+	client.on('connect', function(){
 		io.emit('connect', 'welcome!');
 	});
 });
 server.listen(3000);
 
 var data;
+var t0,t1, yaw;
 if (mpu.initialize()){
 	setInterval(function(){
-		data = mpu.getRotation();
-		console.log(data);
-		io.emit('yaw', data.yaw);
-	}, 5);
+		data = mpu.getQuaternion();
+		if (data && data.x){
+			//console.log(data);
+			//io.emit('quat', JSON.stringify(data));
+			t0 = +2.0 * (data.w*data.x + data.y*data.z);
+			t1 = +1.0 - 2.0 * (Math.pow(data.x,2) + Math.pow(data.y,2));
+			yaw = -Math.atan2(t0,t1);
+			//console.log("emit yaw: " + yaw);
+			io.emit('yaw', yaw);
+		} else {
+			console.log("failed to update!");
+		}
+}, 50);
 }
 
 
 
-//app.get('/', function (req, res) {
-//	res.send(data);
-//})
-//
-//app.listen(3000, function () {
-//	console.log('Example app listening on port 3000!')
-//})
+app.get('/', function (req, res) {
+	res.send(data);
+})
+
+app.listen(9000, function () {
+	console.log('Example app listening on port 3000!')
+})
 
